6eb3cc9044db40415a975a720e6e694bc2ec4543
DCVPS <mansalib2@gmail.com>
2026-07-05 20:34:20 +0000

    Update file.sh

--- file.sh
+++ file.sh
@@ -8,7 +8,7 @@ sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o win11.iso https://software.download.prss.microsoft.com/dbazure/Windows11_InsiderPreview_EnterpriseVL_x64_en-gb_28020_2366.iso?t=4b9fc489-98d3-4ca5-9461-d498207caed3&P1=1783369931&P2=602&P3=2&P4=am3nNkHP9lEBmRw1JdzrWaZ7KFTpxAsir7lRV0gqYl5f7uvuN1B9f2W7rwaWFfZ22v7BQE9V%2b6APG%2fBFbWboTZXOyCJnmNEVYZZkNovrWhOgVLzUhHcF%2fKp10uvSOWOYjQ5owRvPzZkGPlQuNjzr3xAxVgZwd84Ab1u%2fxYVDww%2fzKr45EDv09m4eZg%2bFNWV9fiHe46%2bhradqnMpmwK9VFmmz1xpwi8IaZSXd%2f9U9REPWI2aYB0St%2fbIcDwYFMxWMAaCPUkl%2bqAQF9XEKrCuTPiXd0REWt9Eq%2bG6Qld1eiDeH4QNhGDvc%2boNn5GwuqG0C28AsxeMjXSuE0EgFQLAnTA%3d%3d
+curl -o win11.iso https://software.download.prss.microsoft.com/dbazure/Windows11_InsiderPreview_EnterpriseVL_x64_en-gb_28020_2366.iso?t=4b9fc489-98d3-4ca5-9461-d498207caed3&P1=1783369931&P2=602&P3=2&P4=am3nNkHP9lEBmRw1JdzrWaZ7KFTpxAsir7lRV0gqYl5f7uvuN1B9f2W7rwaWFfZ22v7BQE9V%2b6APG%2fBFbWboTZXOyCJnmNEVYZZkNovrWhOgVLzUhHcF%2fKp10uvSOWOYjQ5owRvPzZkGPlQuNjzr3xAxVgZwd84Ab1u%2fxYVDww%2fzKr45EDv09m4eZg%2bFNWV9fiHe46%2bhradqnMpmwK9VFmmz1xpwi8IaZSXd%2f9U9REPWI2aYB0St%2fbIcDwYFMxWMAaCPUkl%2bqAQF9XEKrCuTPiXd0REWt9Eq%2bG6Qld1eiDeH4QNhGDvc%2boNn5GwuqG0C28AsxeMjXSuE0EgFQLAnTA%3d%3d
 echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
